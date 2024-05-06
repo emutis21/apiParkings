f23b608a2aaf28115192a634af6e08c5695fe0b1
Esteban Mutis <smithmutis21@gmail.com>
2024-05-05 19:51:53 -0500

    Add application.properties, maven-wrapper.properties, ParkingsApiApplicationTests.java, .gitignore, and ParkingsApiApplication.java files

---
+++ src/main/java/com/parkings/parkingsApi/ParkingsApiApplication.java
@@ -0,0 +1,13 @@
+package com.parkings.parkingsApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParkingsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParkingsApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/parkings/parkingsApi/ParkingsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.parkings.parkingsApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParkingsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
